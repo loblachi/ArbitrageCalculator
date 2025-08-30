9d7b9a5aac8b5cc78013bb64074619a221944ade
loblachi <codebasic69@gmail.com>
2025-08-30 02:04:10 +0200

    added some things and Equal feature works

--- jsMain.js
+++ jsMain.js
@@ -27,9 +27,10 @@ function checkIfCoverAvail(Home, Draw, Away){
                          <h4>${arbPercentage.toFixed(2)} %</h4>
                          <h3 style="color:green;">Arbitrage Available</h3>
               </div>
-             `
+             `      
       }
        resultsElem.innerHTML = msg; 
+       displayInputAmounts(arbPercentage);
 }
 
 function checkValidInputs(val1 , val2 , val3){
@@ -42,3 +43,144 @@ function checkValidInputs(val1 , val2 , val3){
     }
     return true; 
 }
+
+function displayInputAmounts(perc){
+    let html; 
+    if( perc < 100){ 
+      html = 
+    `
+    <input type="number"class="amount-input" id="stakeInput" placeholder="Enter total stake (Rands)">
+
+    <div class="radio-div">
+        <label><input type="radio" name="strategy" value="equal" checked> Equal profit</label>
+        <label><input type="radio" name="strategy" value="home"> Home profit</label>
+        <label><input type="radio" name="strategy" value="away"> Away profit</label>
+        <label><input type="radio" name="strategy" value="draw"> Draw profit</label>  
+    </div>
+
+    <button id="calculateBtn" class="calculateBtn">Calculate Bets</button>
+
+    <div id="result"></div>
+    `; 
+    document.querySelector(".section-two-flex").innerHTML = html;
+    document.querySelector("#calculateBtn").addEventListener("click" , () => {
+    verifyTotalAmount();  
+    });
+    } else {
+       html = ``; 
+       document.querySelector(".section-two-flex").innerHTML = html;
+    }
+}
+
+function verifyTotalAmount(){
+    const selected = document.querySelector('input[name="strategy"]:checked').value;
+    const amt = document.querySelector(".amount-input").value ;
+    console.log(selected + " " + amt); 
+
+    calculateProfit(amt , selected)
+}
+function calculateProfit(amt , radioSelected){
+    let homeOdd  = document.querySelector("#odd1").value; 
+    let drawOdd  = document.querySelector("#odd2").value; 
+    let awayOdd  = document.querySelector("#odd3").value;
+
+    const pHome = 1 / homeOdd;
+    const pDraw = 1 / drawOdd;
+    const pAway = 1 / awayOdd;
+
+    const sum = pHome + pDraw + pAway;
+
+    switch(radioSelected){
+        case "equal" :
+             calculateEqualProfit(amt , pHome , pDraw , pAway, sum); 
+        break;
+
+        case "home" :
+
+        break;
+
+        case "away" : 
+
+        break; 
+
+        case "draw" :
+
+        break; 
+
+        default:
+            console.log("Unknown Strategy");
+    }
+}
+function calculateEqualProfit(totalAmount , pHome , pDraw , pAway , sum){
+ // Calculate stakes for equal profit
+  const stakeHome = totalAmount * (pHome / sum);
+  const stakeDraw = totalAmount * (pDraw / sum);
+  const stakeAway = totalAmount * (pAway / sum);
+
+  // Calculate profit (same for all outcomes)
+  const profit = totalAmount * (1 / sum - 1);
+  let arrObj = [
+    {
+        odds: 1/pHome,
+        stake: stakeHome.toFixed(2),
+        return: (stakeHome * (1/pHome)).toFixed(2),
+        profit: ((stakeHome * (1/pHome)) - totalAmount).toFixed(2)
+    },
+    {
+        odds: 1/pAway,
+        stake: stakeAway.toFixed(2),
+        return: (stakeAway * (1/pAway)).toFixed(2),
+        profit: ((stakeAway * (1/pAway)) - totalAmount).toFixed(2)
+    },
+    {
+        odds: 1/pDraw,
+        stake: stakeDraw.toFixed(2),
+        return: (stakeDraw * (1/pDraw)).toFixed(2),
+        profit: ((stakeDraw * (1/pDraw)) - totalAmount).toFixed(2)
+    }
+  ]
+  let html_a = `
+  <table class="table-info">
+  <tr>
+    <th>Odds</th>
+    <th>Initial Stake</th>
+    <th>Total Return</th>
+    <th>Actual Profit</th>
+  </tr>
+  ${iterateOver(arrObj)}
+  </table>
+  `
+  console.log(html_a); 
+  document.querySelector("#result").innerHTML = html_a; 
+}
+
+
+function iterateOver(arrObj){
+    let combinedHtml = ''; 
+    arrObj.forEach( (obj) => {
+       let singleHtml = `
+        <tr>
+            <td>${obj.odds}</td>
+            <td>${obj.stake}</td>
+            <td>${obj.return}</td>
+            <td>${obj.profit}</td>
+        </tr>
+        `; 
+         combinedHtml += singleHtml; 
+  })
+  return combinedHtml; 
+}
+
+
+
+
+
+
+
+/* 
+| Outcome (Odds)   |  Stake (R) |   Return (R) |  Profit (R) |
+| ---------------- | ---------: | -----------: | ----------: |
+| Tottenham (2.20) | **500.00** | **1,100.00** | **+207.78** |
+| Other (4.30)     |     207.49 |       892.22 |        0.00 |
+| Other (4.83)     |     184.72 |       892.22 |        0.00 |
+*/ 
